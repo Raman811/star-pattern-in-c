ebf4831920e8a3a0649c7108fbeb5b963d6e4bee
ramandeep-gurukul <ramandeep-gurukul@users.noreply.github.com>
2018-03-02 16:46:56 +0530

    Add files via upload

---
+++ starpattern10.c
@@ -0,0 +1,24 @@
+main()
+{
+    int i,j,k=0,rows,n;
+    printf("enter no of rows:");
+    scanf("%d",&rows);
+    n=(rows+1)/2;
+    for(i=1;i<=rows;i++)
+    {
+        if(rows%2==0)
+           if(i<n) k++;
+        if(i>n+1) k--;
+            else
+
+        for(j=1;j<=rows;j++)
+        {
+            if(j>=n+1-k&&j<=n-1+k)
+                printf("*");
+            else
+                printf(" ");
+
+        }
+        printf("\n");
+    }
+}
